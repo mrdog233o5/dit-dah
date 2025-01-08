3ee6144f0f1c828141f4296b260ab1049a693ad2
mrdog233o5 <mr.dog233o5@gmail.com>
2025-01-08 21:01:58 +0800

    commit

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import sys
-import threading 
+import threading
+import keyboard
+import time
+import morse
+KEY = "shift"
+KEY_LENGTH = 0.1
+lastPress = 0
+lastRelease = 0
+keyUp = True
+phrase = []
+char = ""
+letters = []
 IS_MAC = sys.platform == 'darwin'
 IS_WIN = sys.platform == 'win32'
 if IS_MAC:
@@ -7,81 +18,67 @@ if IS_MAC:
 # else:
     # from ruwps import *
 
-def main():
 
-    def e(_):
-        print('EEEEEEE')
+def e(_):
+    print('EEEEEEE')
 
-    icon_file = 'favIcon.png'
-    app = App('lovegun', icon=icon_file, template=True)
-    app.menu = [
-        None
-    ]
+icon_file = 'favIcon.png'
+app = App('lovegun', icon=icon_file, template=True)
+app.menu = [
+    None
+]
 
-    # here
-    import keyboard
-    import time
-    import morse
-    KEY = "shift"
-    KEY_LENGTH = 0.1
-    lastPress = 0
-    lastRelease = 0
+# here
+
+def press(e):
+    print("nigga")
+    global keyUp
+    if not keyUp:
+        return
+    global lastPress
+    lastPress = time.time()
+    keyUp = False
+
+def release(e):
+    global keyUp
+    global lastRelease
+    global KEY_LENGTH
+    global char
+    lastRelease = time.time()
+    timeLength = lastRelease - lastPress
     keyUp = True
-    phrase = []
+
+    temp = [length-1 for length in range(1,3) if abs(timeLength - length*KEY_LENGTH) < 0.05 or timeLength < KEY_LENGTH]
+    if len(temp) == 0:
+        length = 1
+    else:
+        length = temp[0]
+
+    if length:
+        char += "-"
+    else:
+        char += "."
+
+def write():
+    global char
+    global phrase
+    keyboard.write(morse.m2chr(char).lower())
     char = ""
-    letters = []
+    phrase = []
 
-    def press(e):
-        global keyUp
-        if not keyUp:
-            return
-        global lastPress
-        lastPress = time.time()
-        keyUp = False
+def checkTime():
+    global char
+    if time.time() - lastRelease > KEY_LENGTH*4 and len(char) > 0:
+        write()
 
-    def release(e):
-        global keyUp
-        global lastRelease
-        global KEY_LENGTH
-        global char
-        lastRelease = time.time()
-        timeLength = lastRelease - lastPress
-        keyUp = True
+keyboard.on_press_key(KEY, press)
+keyboard.on_release_key(KEY, release)
 
-        temp = [length-1 for length in range(1,3) if abs(timeLength - length*KEY_LENGTH) < 0.05 or timeLength < KEY_LENGTH]
-        if len(temp) == 0:
-            length = 1
-        else:
-            length = temp[0]
+def menuLoop():
+    while 1:
+        checkTime()
 
-        if length:
-            char += "-"
-        else:
-            char += "."
+menuThread = threading.Thread(target=menuLoop) 
 
-    def write():
-        global char
-        global phrase
-        keyboard.write(morse.m2chr(char).lower())
-        char = ""
-        phrase = []
-
-    def checkTime():
-        global char
-        if time.time() - lastRelease > KEY_LENGTH*4 and len(char) > 0:
-            write()
-
-    keyboard.on_press_key(KEY, press)
-    keyboard.on_release_key(KEY, release)
-
-    def menuLoop():
-        while 1:
-            checkTime()
-
-    menuThread = threading.Thread(target=menuLoop) 
-
-    menuThread.start()
-    app.run()
-
-if __name__ == "__main__":
-    main()
+menuThread.start()
+app.run()
